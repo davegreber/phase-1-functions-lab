a5c4d75efab1df8e81c0a55de9a2b15b26cfa752
Dave Greber <davegreber@gmail.com>
2022-09-20 19:29:39 -0400

    commit tree dc9cc54f86080b5820c44f4b34c4eda5539c84c4

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+/* function 1 
+distanceFromHqInBlocks: 
+Returns the number of blocks from Scuber's headquarters to the pickup location. */
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    return Math.abs(someValue - 42)
+    }
+    
+
+
+//function 2
+// distanceFromHqInFeet
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+    }
+
+//function 3
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(destination - start) * 264
+}
+
+//function 4
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0
+    }
+    if (distance>=400 && distance<=2000){
+        return (distance-400)* 0.02
+    }
+    else if(distance >= 2000 && distance<=2500){
+        return 25
+    }
+    else if (distance > 2500){
+        return "cannot travel that far"
+    }
+  }
+
